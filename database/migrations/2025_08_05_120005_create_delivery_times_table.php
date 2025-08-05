2a356d9ebff6372032a92571ea50bae866b0817c
倉松惇 <kuramatsujun@kuramatsuatsushinoMacBook-Pro.local>
2025-08-05 21:16:08 +0900

    curriculumsテーブル、delivery_timesテーブル作成

---
+++ database/migrations/2025_08_05_120005_create_delivery_times_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery_times', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('curriculums_id')->nullable();
+            $table->dateTime('delivery_from')->nullable();
+            $table->dateTime('delivery_to')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery_times');
+    }
+};
