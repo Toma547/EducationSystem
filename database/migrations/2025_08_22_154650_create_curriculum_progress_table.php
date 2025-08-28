77a55f67b03d250e7baa1460fde10329528d9599
f-misumi <lapisalba84@gmail.com>
2025-08-29 06:53:40 +0900

    curriculum_progressテーブル,users追加フィールドのマイグレーション作成

---
+++ database/migrations/2025_08_22_154650_create_curriculum_progress_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculum_progress', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            // $table->foreignId('curriculum_id')->constrained('curriculums')->cascadeOnDelete();
+
+            $table->boolean('clear_flg')->default(false)->comment('0:未クリア, 1:クリア');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculum_progress');
+    }
+};
