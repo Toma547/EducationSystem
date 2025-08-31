cb484afd506e6308a22cbef0a51e154caa0fc01c
松尾晏奈 <anna@matsuoannas-MacBook-Air.local>
2025-09-01 08:31:41 +0900

    Add my curriculum and delivery views/controllers

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 管理画面ルート
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->group(function () {
+
+        // ログイン画面
+        Route::get('/login', 'LoginController@showLoginForm')->name('show.login');
+
+        // ユーザー新規登録画面
+        Route::get('/register', 'RegisterController@showRegisterForm')->name('show.register');
+
+        // トップページ
+        Route::get('/top', 'TopController@showTop')->name('show.top');
+
+        // 授業一覧画面
+        Route::get('/curriculum_list', 'CurriculumController@showCurriculumList')->name('show.curriculum.list');
+
+        // 授業新規登録画面
+        Route::get('/curriculum_create', 'CurriculumController@showCurriculumCreate')->name('show.curriculum.create');
+
+        // 授業編集画面
+        Route::get('/curriculum_edit/{id}', 'CurriculumController@showCurriculumEdit')->name('show.curriculum.edit');
+
+        // 配信日時設定画面
+        Route::get('/delivery_edit/{id}', 'DeliveryController@showDeliveryEdit')->name('show.delivery.edit');
+
+        // お知らせ一覧画面
+        Route::get('/article_list', 'ArticleController@showArticleList')->name('show.article.list');
+
+        // お知らせ新規登録画面
+        Route::get('/article_create', 'ArticleController@showArticleCreate')->name('show.article.create');
+
+        // お知らせ編集画面
+        Route::get('/article_edit/{id}', 'ArticleController@showArticleEdit')->name('show.article.edit');
+
+        // バナー設定画面
+        Route::get('/banner_edit', 'BannerController@showBannerEdit')->name('show.banner.edit');
+});
